0dfdb018c2984475aae003e2641dd2b9d44a15bc
KillyBOT <edwards.kyle.j@gmail.com>
2020-03-23 09:41:58 -0400

    I added an 'octave' feature where you can create smoother and more realistic looking gradients by adding up different versions of gradients with differing weights

--- main.c
+++ main.c
@@ -4,21 +4,29 @@
 
 #include "pnoise.h"
 
+#define SIZE 1000
+#define RES 32
+
 int main(){
 	FILE* f = fopen("testImage.ppm","w");
 
-	fputs("P3\n1024 1024\n255\n",f);
+	char header[64];
+
+	sprintf(header, "P3\n%d %d\n255\n",SIZE,SIZE);
+
+	fputs(header,f);
 
 	double xf,yf;
 	int final;
 	char grad[8];
 
-	for(int y = 0; y < 1024; y++){
-		for(int x = 0; x < 1024; x++){
-			xf = (double)x / 32;
-			yf = (double)y / 32;
+	for(int y = 0; y < SIZE; y++){
+		for(int x = 0; x < SIZE; x++){
+			xf = (double)x / RES;
+			yf = (double)y / RES;
 
-			final = 255 * perlin(xf, yf, 0);
+			//final = 255 * perlin(xf, yf, 0);
+			final = 255 * perlin_octave(xf,yf,0,4,.5);
 
 			sprintf(grad, "%d ", final);
 
--- pnoise.h
+++ pnoise.h
@@ -4,6 +4,7 @@
 #define REPEAT 256
 
 double perlin(double x, double y, double z);
+double perlin_octave(double x, double y, double z, int octaves, double persistence);
 double grad(int hash, double x, double y, double z);
 double fade(double t);
 double lerp(double a, double b, double x);
